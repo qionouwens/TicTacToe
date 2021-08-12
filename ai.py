590b48d97fd91d6603c40fd0e2eacb1336024914
qionouwens <qionouwens@gmail.com>
2021-08-12 17:13:33 +0200

    Added an ai containing several ai function without any code yet

---
+++ ai.py
@@ -0,0 +1,22 @@
+def random_ai(board):
+    pass
+
+
+def random_with_finish_ai(board):
+    pass
+
+
+def random_start_conors_ai(board):
+    pass
+
+
+def random_start_middle_ai(board):
+    pass
+
+
+def block_and_finish_ai(board):
+    pass
+
+
+def min_max_ai(board):
+    pass
